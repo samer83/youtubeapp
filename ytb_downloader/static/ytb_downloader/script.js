490f9b13626629819d4d867e86a927d6044f304d
samer83 <55295850+samer83@users.noreply.github.com>
2020-07-17 21:58:08 +0300

    static files / gitignore / migrations

---
+++ ytb_downloader/static/ytb_downloader/script.js
@@ -0,0 +1,30 @@
+$(function() {
+    
+    $('#download').click(function(e) {
+        e.preventDefault()
+    var $crf_token = $('[name="csrfmiddlewaretoken"]').attr('value');
+
+    let formData = {
+        'url'      : $('#inputurl').val(),
+    };
+    $.ajax({
+        url:"/api/downloader/youtube/",
+        async:true,
+        headers:{"X-CSRFToken": $crf_token},
+        dataType: "json",
+        type: "POST",
+        data: formData,
+        success: function( data ) {
+            if (data['error'] == "0"){
+                var name = data['name']
+                window.open('http://localhost:8000/static/' + name)
+
+            }
+        },
+        error: function(data){
+            console.log(data)
+        }
+    });
+});
+
+});
